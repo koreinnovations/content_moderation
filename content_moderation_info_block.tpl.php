fb7045988e5fc1eca8f362b144c9059784c084b1
Eugen Mayer <eugen.mayer@kontextwork.de>
2010-01-14 19:57:01 +0000

    Adding: http://drupal.org/node/684780 Cleaning up the UI of the info block a lot Fixed: http://drupal.org/node/684806 Fixed: http://drupal.org/node/684726 Fixed: menu_get_object does not reflect the correct node if you are viewing a revision Update: upddating translation template Update: updaing german translation

--- content_moderation_info_block.tpl.php
+++ content_moderation_info_block.tpl.php
@@ -12,28 +12,43 @@ $user = user_load($live->revision_uid);
 
 $edit_icon = '<span class="tinyicon t_editicon"></span>';
 $rev_icon = '<span class="tinyicon t_revisionicon"></span>';
+$edit_state_icon = '<span class="tinyicon t_changestateicon"></span>';
 $delete_icon = '<span class="tinyicon t_deleteicon"></span>';
+$view_icon = '<span class="tinyicon t_viewicon"></span>';
 
 /* actions */
-$edit_state = l($edit_icon,$links['edit_state'],array('html' => true, 'attributes' => array( 'title' => t('Edit the state of this revision like review or approval') ) ));
+if(_content_moderation_statechange_allowed($node->vid)) {
+  $edit_state = l($edit_state_icon,$links['edit_state'],array('html' => true, 'attributes' => array( 'title' => t('Edit the state of this revision like review or approval') ) ));
+}
 $edit_live = l($edit_icon,$links['edit_live'],array('html' => true, 'attributes' => array( 'title' => t('Edit the moderation status of this revision') ) ));
+
+$view_live_link = l($view_icon,$links['live_view'],array('html' => true, 'attributes' => array( 'title' => t('View live revision') ) ));
+$view_current_link = l($view_icon,"node/{$live->nid}/revisions/{$node->vid}/view",array('html' => true, 'attributes' => array( 'title' => t('View this revision') ) ));
+
 if(module_exists('diff')){
   $compare_live = l($rev_icon,$links['compare_with_live'],array('html' => true, 'attributes' => array( 'title' => t('Compare this revision with the live revision') ) ));
   $compare = l($rev_icon,$links['compare'],array('html' => true, 'attributes' => array( 'title' => t('List all revision') ) ));
 }
-
-//$edit_current = l($edit_icon,$links['edit_revision'],array('html' => true, 'attributes' => array( 'title' => t('Edit this revision') ) ));
-$delete_current = l($delete_icon,$links['delete_revision'],array('html' => true, 'attributes' => array( 'title' => t('Delete this revision') ) ));
+// TODO: first see how revision deleting, esp. the live ones, should be handled
+//$delete_current = l($delete_icon,$links['delete_revision'],array('html' => true, 'attributes' => array( 'title' => t('Delete this revision') ) ));
 
 ?>
 <div id="content_moderation">
-   <div class="info live_info"><?=t('Live')?>: <?=$live_link?> <?=$edit_live?> <?=$compare?><br/>
+  <h4><?=t('Live')?></h4>
+   <div class="info live_info"><?=$live->vid?>: <?=$live_link.$view_live_link.$edit_live.$compare?><br/>
     <span class="details">&raquo; <?=date('d.m.y',$live->changed)?> (<?=$user->name?>)</span>
   </div>
   <?php if($node->vid != $live->vid) {?>
-  <div class="info"><label><?=t('Revision')?>: </label> <?=l($node->vid,'node/'.$live->nid.'/'.$node->vid)?> <?=$edit_current?> <?=$compare_live?> <?=$delete_current?><br/>
+  <h4><?=t('Viewing')?></h4>
+  <div class="info"><label><?=$node->vid?>:</label> (<?=$state?>) <?=$view_current_link.$edit_state.$compare_live.$delete_current?><br/>
     <span class="details">&raquo; <?=date('d.m.y',$live->revision_timestamp)?> (<?=$user->name?>)</span>
+    <span class="details state">&raquo; Status: <?=$state?></span>
   </div>
-    <div class="current_state info"><label>Status: </label><?=$state?> <?if(_content_moderation_statechange_allowed($node->vid)) { echo $edit_state;}?></div>
   <?php }?>
+
+  <?if($revisions_list != "") { ?>
+  <h4><?=t('Pending')?></h4>
+  <?php
+      echo $revisions_list;
+   } ?>
 </div>
---
+++ content_moderation_revisions_list.tpl.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * Usable values
+ * $node : currently viewed node
+ * $state: workflow state of the currently viewed node revision
+ * $live: live version of the node
+ * $live_link: revision and link to the live node
+ * $links: actions on the different revisions (compare_with_live,compare,delete_revision, edit_revision, edit_live)
+ */
+?>
+<div class='revision_list'>
+<?
+
+foreach($revisions as $rev) {
+  $edit_icon = '<span class="tinyicon t_editicon"></span>';
+  $rev_icon = '<span class="tinyicon t_revisionicon"></span>';
+  $view_icon = '<span class="tinyicon t_viewicon"></span>';
+  $edit_state_icon = '<span class="tinyicon t_changestateicon"></span>';
+
+  $view_link = l($view_icon,"node/{$rev->nid}/revisions/{$rev->vid}/view",array('html' => true, 'attributes' => array( 'title' => t('View revision @rev',array('@rev' => $rev->vid) ))));
+
+  if(_content_moderation_statechange_allowed($rev->vid)) {
+    $edit_state_link = l($edit_state_icon,_content_moderation_change_state_link($rev->vid,$rev->nid),array('html' => true, 'attributes' => array( 'title' => t('Change state of revision @rev',array('@rev' => $rev->vid) ))));
+  }
+  if(module_exists('diff')){
+    $live = _content_moderation_live_revision($node->nid);
+    $compare_live = l($rev_icon,"node/{$rev->nid}/revisions/view/{$live->vid}/{$rev->vid}",array('html' => true, 'attributes' => array( 'title' => t('Compare this revision with the live revision') ) ));
+    $compare = l($rev_icon,$links['compare'],array('html' => true, 'attributes' => array( 'title' => t('List all revision') ) ));
+  }
+  $compare = 'compare';
+
+  if($rev->state != NULL) {
+    $state = ucfirst(t($rev->state));
+  }
+  else {
+    $state  = ucfirst(t('None'));
+  }
+?>
+<span class="revision"><?=$rev->vid?>: (<?=$state?>) <?=$view_link?><?=$edit_state_link?><?=$compare_live?></span>
+<?php
+}
+?>
+</div>
