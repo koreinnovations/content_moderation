7222c16e6143797481dfc0c644909bd2ca145ebc
Eugen Mayer <eugen.mayer@kontextwork.de>
2010-04-18 11:31:29 +0000

    Added: (EugenMayer) [#773164] Added: (EugenMayer) [#762790] Added: (EugenMayer) [#729212] Added: (freakalis, sin.star, EugenMayer) [#770070] Added: (sin.star) [#771270] Added: (sin.star, EugenMayer) [#58710] Added: (zenned) [#757152] Fixed: (duozersk, EugenMayer] [#763168] Fixed: (chellman) [#750880] Fixed: (akhaef) [#762112] Fixed: (wesley_2mpact) [#758122] Fixed: (sin.star) [#758238] Fixed: (derein) Code formatting Fixed: (EugenMayer) [#768758] Updated: (EugenMayer)German translation

--- content_moderation_info_block.tpl.php
+++ content_moderation_info_block.tpl.php
@@ -12,7 +12,8 @@
  * $links: actions on the different revisions (compare_with_live,compare,delete_revision, edit_revision, edit_live)
  */
 
-$user = user_load($live->revision_uid);
+
+$current_user = user_load($node->revision_uid);
 
 $edit_icon = '<span class="tinyicon t_editicon"></span>';
 $rev_icon = '<span class="tinyicon t_revisionicon"></span>';
@@ -20,14 +21,19 @@ $edit_state_icon = '<span class="tinyicon t_changestateicon"></span>';
 $delete_icon = '<span class="tinyicon t_deleteicon"></span>';
 $view_icon = '<span class="tinyicon t_viewicon"></span>';
 
+
 /* actions */
 if(_content_moderation_statechange_allowed($node->vid)) {
-  $edit_state = l($edit_state_icon,$links['edit_state'],array('html' => true, 'attributes' => array( 'title' => t('Edit the state of this revision (review, approve).') ) ));
+  $edit_state = l($edit_state_icon,$links['edit_state'],array('html' => true, 'attributes' => array( 'title' => t('Edit the state of this revision.') ) ));
 }
-$edit_live = l($edit_icon,$links['edit_live'],array('html' => true, 'attributes' => array( 'title' => t('Edit the moderation status of this revision.') ) ));
 
-$view_live_link = l($view_icon,$links['live_view'],array('html' => true, 'attributes' => array( 'title' => t('View live revision') ) ));
-$view_current_link = l($view_icon,"node/{$live->nid}/revisions/{$node->vid}/view",array('html' => true, 'attributes' => array( 'title' => t('View this revision.') ) ));
+if($live != NULL) {
+  $view_live_link = l($view_icon,$links['live_view'],array('html' => true, 'attributes' => array( 'title' => t('View live revision') ) ));
+
+}
+
+$view_current_link = l($view_icon,"node/{$node->nid}/revisions/{$node->vid}/view",array('html' => true, 'attributes' => array( 'title' => t('View this revision.') ) ));
+$current_rev_link = l($node->vid,"node/{$node->nid}/revisions/{$node->vid}/view",array('html' => true, 'attributes' => array( 'title' => t('View this revision.') ) ));
 
 if(module_exists('diff')){
   $compare_live = l($rev_icon,$links['compare_with_live'],array('html' => true, 'attributes' => array( 'title' => t('Compare this revision with the live revision.') ) ));
@@ -38,20 +44,31 @@ if(module_exists('diff')){
 
 ?>
 <div id="content_moderation">
+
   <h4><?php print t('Live')?></h4>
-   <div class="info live_info"><?php print $live->vid?>: <?=$live_link.$view_live_link.$edit_live.$compare?><br/>
-    <span class="details">&raquo; <?php print date('d.m.y',$live->changed)?> (<?php print $user->name?>)</span>
-  </div>
-  <?php if($node->vid != $live->vid) {?>
+  <?php if($live != NULL) {
+    $live_user = user_load($live->revision_uid);
+    ?>
+    <div class="info live_info"><?php print $live->vid?>: <?=$live_link.$view_live_link.$compare?><br/>
+    <span class="details">&raquo; <?php print format_date($live->revision_timestamp, 'small')?> (<?php print $live_user->name?>)</span>
+    </div>
+  <?php }  else { ?>
+    <div class="info live_info"><?php print t('nothing approved yet');?></div>
+  <?php } ?>
+
+
+  <?php if($live == NULL || $node->vid != $live->vid) {?>
   <h4><?php print t('Viewing')?></h4>
-  <div class="info"><label><?php print $node->vid?>:</label> (<?php print $state?>) <?php print $view_current_link.$edit_state.$compare_live.$delete_current?><br/>
-    <span class="details">&raquo; <?php print date('d.m.y',$live->revision_timestamp)?> (<?php print $user->name?>)</span>
+  <div class="info"><label><?php print $current_rev_link?>:</label> (<?php print $state?>) <?php print $view_current_link.$edit_state.$compare_live.$delete_current?><br/>
+    <span class="details">&raquo; <?php print format_date($node->revision_timestamp, 'small')?> (<?php print $current_user->name?>)</span>
     <span class="details state">&raquo; Status: <?php print t($state)?></span>
   </div>
   <?php }?>
 
   <?php if($revisions_list != "") { ?>
   <h4><?php print t('Pending')?></h4>
-   <?php echo $revisions_list;
-   } ?>
+     <div class="info">
+        <?php echo $revisions_list; ?>
+     </div>
+   <?php } ?>
 </div>
--- content_moderation_revisions_list.tpl.php
+++ content_moderation_revisions_list.tpl.php
@@ -11,23 +11,34 @@
  * $live_link: revision and link to the live node
  * $links: actions on the different revisions (compare_with_live,compare,delete_revision, edit_revision, edit_live)
  */
+
+if(!is_array($revisions) || count($revisions) == 0) {
+  return "";
+}
 ?>
 <div class='revision_list'>
 <?php
+$edit_icon = '<span class="tinyicon t_editicon"></span>';
+$rev_icon = '<span class="tinyicon t_revisionicon"></span>';
+$view_icon = '<span class="tinyicon t_viewicon"></span>';
+$edit_state_icon = '<span class="tinyicon t_changestateicon"></span>';
+
 foreach($revisions as $rev) {
-  $edit_icon = '<span class="tinyicon t_editicon"></span>';
-  $rev_icon = '<span class="tinyicon t_revisionicon"></span>';
-  $view_icon = '<span class="tinyicon t_viewicon"></span>';
-  $edit_state_icon = '<span class="tinyicon t_changestateicon"></span>';
+  $edit_state_link = '';
+  $compare_live = '';
+  $compare = '';
 
   $view_link = l($view_icon,"node/{$rev->nid}/revisions/{$rev->vid}/view",array('html' => true, 'attributes' => array( 'title' => t('View revision @rev.',array('@rev' => $rev->vid) ))));
+  $revision_link = l($rev->vid,"node/{$rev->nid}/revisions/{$rev->vid}/view",array('html' => true, 'attributes' => array( 'title' => t('View revision @rev.',array('@rev' => $rev->vid) ))));
 
-  if(_content_moderation_statechange_allowed($rev->vid)) {
+  if(_content_moderation_statechange_allowed($rev->vid) !== false) {
     $edit_state_link = l($edit_state_icon,_content_moderation_change_state_link($rev->vid,$rev->nid),array('html' => true, 'attributes' => array( 'title' => t('Change state of revision @rev.',array('@rev' => $rev->vid) ))));
   }
   if(module_exists('diff')){
     $live = _content_moderation_live_revision($node->nid);
-    $compare_live = l($rev_icon,"node/{$rev->nid}/revisions/view/{$live->vid}/{$rev->vid}",array('html' => true, 'attributes' => array( 'title' => t('Compare this revision with the current live revision.') ) ));
+    if($live != NULL) {
+      $compare_live = l($rev_icon,"node/{$rev->nid}/revisions/view/{$live->vid}/{$rev->vid}",array('html' => true, 'attributes' => array( 'title' => t('Compare this revision with the current live revision.') ) ));
+    }
     $compare = l($rev_icon,$links['compare'],array('html' => true, 'attributes' => array( 'title' => t('List all revisions.') ) ));
   }
   $compare = 'compare';
@@ -39,7 +50,7 @@ foreach($revisions as $rev) {
     $state  = ucfirst(t('none'));
   }
 ?>
-<span class="revision"><?php print $rev->vid?>: (<?php print $state?>) <?php print $view_link?><?php print $edit_state_link?><?php print $compare_live?></span>
+<span class="revision"><?php print $revision_link?>: (<?php print $state?>) <?php print $view_link?><?php print $edit_state_link?><?php print $compare_live?></span>
 <?php
 }
 ?>
